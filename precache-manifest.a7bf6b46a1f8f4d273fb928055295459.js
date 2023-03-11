0e4d4639bcd71c5a02ed11abf9954d0a81a060d2
aldoprogrammer <aldols@bugcrowdninja.com>
2023-03-11 15:17:51 +0700

    Updates

---
+++ precache-manifest.a7bf6b46a1f8f4d273fb928055295459.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb246125eb2a97ecb6b6",
+    "url": "/portofolio-aldo/static/css/main.540765a3.chunk.css"
+  },
+  {
+    "revision": "cb246125eb2a97ecb6b6",
+    "url": "/portofolio-aldo/static/js/main.cb246125.chunk.js"
+  },
+  {
+    "revision": "4c9aa3f9f967913d2422",
+    "url": "/portofolio-aldo/static/js/1.4c9aa3f9.chunk.js"
+  },
+  {
+    "revision": "910dff73604ac3425ccd",
+    "url": "/portofolio-aldo/static/css/2.0128d00f.chunk.css"
+  },
+  {
+    "revision": "910dff73604ac3425ccd",
+    "url": "/portofolio-aldo/static/js/2.910dff73.chunk.js"
+  },
+  {
+    "revision": "9b49c328a073b67db0b2",
+    "url": "/portofolio-aldo/static/js/runtime~main.9b49c328.js"
+  },
+  {
+    "revision": "592cc9015410daf3e9f9a1e921798ef3",
+    "url": "/portofolio-aldo/static/media/1.592cc901.jpg"
+  },
+  {
+    "revision": "86a0e23f5b03045c2e2a242f8af9337a",
+    "url": "/portofolio-aldo/static/media/2.86a0e23f.jpg"
+  },
+  {
+    "revision": "114b4c9a57b41ef8bc799247f16ea25f",
+    "url": "/portofolio-aldo/static/media/3.114b4c9a.jpg"
+  },
+  {
+    "revision": "2b7c5d5070e9de47753b172f870b158e",
+    "url": "/portofolio-aldo/static/media/4.2b7c5d50.jpg"
+  },
+  {
+    "revision": "bbe4072d96219d23878a327f9f0f327d",
+    "url": "/portofolio-aldo/static/media/5.bbe4072d.jpg"
+  },
+  {
+    "revision": "f452ac976e15f1115e369799c722b783",
+    "url": "/portofolio-aldo/static/media/6.f452ac97.jpg"
+  },
+  {
+    "revision": "96a5024aa816a0fd0e9e64974b138f65",
+    "url": "/portofolio-aldo/index.html"
+  }
+];
